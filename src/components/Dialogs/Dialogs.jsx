9d4dbc69782f62376666fe21b75ae8760c48951c
betomex <thebetomex@gmail.com>
2021-06-27 23:31:21 +0300

    Dialogs coding

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import './Dialogs.css';
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className="dialogs">
+      <div className="dialogs-items">
+        <div className="dialog active">
+          Ilya
+        </div>
+        <div className="dialog">
+          Makson
+        </div>
+        <div className="dialog">
+          Oks
+        </div>
+        <div className="dialog">
+          Ekaterina
+        </div>
+        <div className="dialog">
+          Andrey
+        </div>
+      </div>
+      <div className="messages">
+        <div className="message">What a nice day today</div>
+        <div className="message">What do you think about going for a walk?</div>
+        <div className="message">It's the best way to relax</div>
+      </div>
     </div>
   );
 }
