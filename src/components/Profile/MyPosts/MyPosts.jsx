ba8200a66b65d3d6b50a0065a38c01966f5d1160
betomex <thebetomex@gmail.com>
2021-07-03 21:18:08 +0300

    ref, onClick

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,15 +7,25 @@ const MyPosts = (props) => {
   let postsElements = props.posts
     .map(post => <Post message={post.message} likesCount={post.likesCount} />)
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    newPostElement.current.value = '';
+    alert(text);
+  }
+
   return (
     <div className="postsBlock">
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add new post</button>
+          <button onClick={() => {
+            addPost()
+          }}>Add new post</button>
         </div>
       </div>
       <div className="posts">
