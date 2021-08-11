994b755bc6c8a5d018b5acb6b647e2e9afd19940
betomex <thebetomex@gmail.com>
2021-08-11 19:55:02 +0300

    users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 import './Users.css';
+import * as axios from "axios";
+import userAvatar from '../../assets/images/98-988072_go-to-image-add-user-icon-clipart.png';
 
 const Users = (props) => {
-  let usersElements = props.users.map(u => <div key={u.id}>
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(r => {
+        props.setUsers(r.data.items);
+      });
+    }
+  }
+
+  return <div>
+    <button onClick={getUsers}>Get Users</button>
+    {props.users.map(u => <div key={u.id}>
       <span>
         <div>
-          <img src={u.avatarUrl} alt="userLogo" className='userAvatar'/>
+          <img src={u.photos.small != null ? u.photos.small : userAvatar} alt="userLogo" className='userAvatar'/>
         </div>
         <div>
-          {u.followed
-            ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
-            : <button onClick={() => {props.follow(u.id)}}>Follow</button>}
+          {u.followed ? <button onClick={() => {
+            props.unfollow(u.id)
+          }}>Unfollow</button> : <button onClick={() => {
+            props.follow(u.id)
+          }}>Follow</button>}
         </div>
       </span>
       <span>
         <span>
-          <div>{u.fullName}</div>
+          <div>{u.name}</div>
           <div>{u.status}</div>
         </span>
         <span>
-          <div>{u.location.city}</div>
-          <div>{u.location.country}</div>
+          <div>{"u.location.city"}</div>
+          <div>{"u.location.country"}</div>
         </span>
       </span>
-    </div>
-  )
-
-  return <div>
-    {usersElements}
+    </div>)}
   </div>
 }
 
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -2,33 +2,33 @@ const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
 
-let initialState = {
-  users: [
-    {
-      id: 1,
-      fullName: 'Ilya',
-      status: 'intoxicating, is not it?',
-      followed: true,
-      avatarUrl: 'https://sun9-79.userapi.com/impg/GvdIERgy93gHCLF_6jBhI1BCxDmMqQ9P6gYZ8A/t21FX9ZYsgg.jpg?size=2160x2160&quality=96&sign=a0a8a26ee2bea6cb55453e58f671ee06&type=album',
-      location: {country: 'Russia', city: 'Krasnodar'}
-    },
-    {
-      id: 2,
-      fullName: 'Makson',
-      status: 'croc is my life',
-      followed: false,
-      avatarUrl: 'https://sun9-79.userapi.com/impg/GvdIERgy93gHCLF_6jBhI1BCxDmMqQ9P6gYZ8A/t21FX9ZYsgg.jpg?size=2160x2160&quality=96&sign=a0a8a26ee2bea6cb55453e58f671ee06&type=album',
-      location: {country: 'Russia', city: 'Krasnodar'}
-    },
-    {
-      id: 3,
-      fullName: 'Oks',
-      status: 'i am proud of my boyfriend',
-      followed: true,
-      avatarUrl: 'https://sun9-79.userapi.com/impg/GvdIERgy93gHCLF_6jBhI1BCxDmMqQ9P6gYZ8A/t21FX9ZYsgg.jpg?size=2160x2160&quality=96&sign=a0a8a26ee2bea6cb55453e58f671ee06&type=album',
-      location: {country: 'Russia', city: 'Krasnodar'}
-    },
-  ]
+let initialState = { users: []
+  // users: [
+  //   {
+  //     id: 1,
+  //     fullName: 'Ilya',
+  //     status: 'intoxicating, is not it?',
+  //     followed: true,
+  //     avatarUrl: 'https://sun9-79.userapi.com/impg/GvdIERgy93gHCLF_6jBhI1BCxDmMqQ9P6gYZ8A/t21FX9ZYsgg.jpg?size=2160x2160&quality=96&sign=a0a8a26ee2bea6cb55453e58f671ee06&type=album',
+  //     location: {country: 'Russia', city: 'Krasnodar'}
+  //   },
+  //   {
+  //     id: 2,
+  //     fullName: 'Makson',
+  //     status: 'croc is my life',
+  //     followed: false,
+  //     avatarUrl: 'https://sun9-79.userapi.com/impg/GvdIERgy93gHCLF_6jBhI1BCxDmMqQ9P6gYZ8A/t21FX9ZYsgg.jpg?size=2160x2160&quality=96&sign=a0a8a26ee2bea6cb55453e58f671ee06&type=album',
+  //     location: {country: 'Russia', city: 'Krasnodar'}
+  //   },
+  //   {
+  //     id: 3,
+  //     fullName: 'Oks',
+  //     status: 'i am proud of my boyfriend',
+  //     followed: true,
+  //     avatarUrl: 'https://sun9-79.userapi.com/impg/GvdIERgy93gHCLF_6jBhI1BCxDmMqQ9P6gYZ8A/t21FX9ZYsgg.jpg?size=2160x2160&quality=96&sign=a0a8a26ee2bea6cb55453e58f671ee06&type=album',
+  //     location: {country: 'Russia', city: 'Krasnodar'}
+  //   },
+  // ]
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -58,7 +58,7 @@ const usersReducer = (state = initialState, action) => {
     case SET_USERS: {
       return {
         ...state,
-        users: [...state.users, action.users]
+        users: [...state.users, ...action.users]
       }
     }
     default:
