463d2e7f16a348eca90bba93e12447093f13c392
betomex <thebetomex@gmail.com>
2021-08-23 20:55:16 +0300

    reselect

--- src/redux/usersSelectors.js
+++ src/redux/usersSelectors.js
@@ -1,6 +1,13 @@
-export const getUsersSelector = (state) => {
+import {createSelector} from "reselect";
+
+const getUsers = (state) => {
   return state.usersPage.users;
 }
+
+export const getUsersSelector = createSelector(getUsers,
+  (users) => {
+  return users.filter(u => true);
+});
 
 export const getPageSizeSelector = (state) => {
   return state.usersPage.pageSize
