3e37d1a688937b97d3b6d5ecc98e1580f8d71e3b
betomex <thebetomex@gmail.com>
2021-09-08 15:07:02 +0300

    React Lazy and Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {Route, withRouter} from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderComponent from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/common/Preloader/Preloader";
+
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
 
 class App extends React.Component {
   componentDidMount() {
@@ -27,18 +28,20 @@ class App extends React.Component {
       <HeaderComponent/>
       <Navbar/>
       <div className='app-wrapper-content'>
-        <Route exact path="/dialogs" render={() =>
-          <DialogsContainer/>
-        }/>
-        <Route path="/profile/:userID?" render={() =>
-          <ProfileContainer/>
-        }/>
-        <Route path="/users" render={() =>
-          <UsersContainer/>
-        }/>
-        <Route path="/login" render={() =>
-          <Login/>
-        }/>
+        <React.Suspense fallback={<Preloader/>}>
+          <Route exact path="/dialogs" render={() =>
+            <DialogsContainer/>
+          }/>
+          <Route path="/profile/:userID?" render={() =>
+            <ProfileContainer/>
+          }/>
+          <Route path="/users" render={() =>
+            <UsersContainer/>
+          }/>
+          <Route path="/login" render={() =>
+            <Login/>
+          }/>
+        </React.Suspense>
       </div>
     </div>
   }
